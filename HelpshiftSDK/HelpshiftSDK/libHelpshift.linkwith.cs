64d1c94b23029fd66dc74aa146595f950681194f
Martin Peschke <martin@hackandcraft.com>
2014-08-20 18:50:56 +0200

    add some frameworks and linkerflags

--- HelpshiftSDK/HelpshiftSDK/libHelpshift.linkwith.cs
+++ HelpshiftSDK/HelpshiftSDK/libHelpshift.linkwith.cs
@@ -1,4 +1,5 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libHelpshift.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "Foundation UIKit")]
+[assembly: LinkWith ("libHelpshift.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, 
+	Frameworks = "Foundation UIKit CoreGraphics QuartzCore CoreText SystemConfiguration CoreTelephony", LinkerFlags="-lsqlite3.0 -llibz")]

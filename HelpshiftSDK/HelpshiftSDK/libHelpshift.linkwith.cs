cd3d06bc30915eb26bebb4d29370e074b2d96424
Martin Peschke <martin@hackandcraft.com>
2014-08-20 19:00:29 +0200

    libz linker flag typo

--- HelpshiftSDK/HelpshiftSDK/libHelpshift.linkwith.cs
+++ HelpshiftSDK/HelpshiftSDK/libHelpshift.linkwith.cs
@@ -2,4 +2,4 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libHelpshift.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, 
-	Frameworks = "Foundation UIKit CoreGraphics QuartzCore CoreText SystemConfiguration CoreTelephony", LinkerFlags="-lsqlite3.0 -llibz")]
+	Frameworks = "Foundation UIKit CoreGraphics QuartzCore CoreText SystemConfiguration CoreTelephony", LinkerFlags="-lsqlite3.0 -lz")]
